687c243902e8f1b8a2df738234331bdac688e646
LiuFeiFei <851085835@qq.com>
2016-08-25 16:15:18 +0800

    123

--- SFoofSave/MVC/SearchFinder/controller/FlowDateChooseViewController.m
+++ SFoofSave/MVC/SearchFinder/controller/FlowDateChooseViewController.m
@@ -7,17 +7,20 @@
 //
 
 #import "FlowDateChooseViewController.h"
+
 #import "LFFPickerVIew.h"
 
-
-@interface FlowDateChooseViewController ()
+@interface FlowDateChooseViewController ()<LFFPickerViewDelegate>
 {
-    UIDatePicker *dataPicker;
+
     LFFPickerVIew *pickerView;
     
 
 }
+@property (weak, nonatomic) IBOutlet UILabel *startTImeL;
 @property (weak, nonatomic) IBOutlet UIButton *startTime; //开始时间
+
+@property (weak, nonatomic) IBOutlet UILabel *overTimeL;
 @property (weak, nonatomic) IBOutlet UIButton *overTime; //结束时间
 
 
@@ -31,10 +34,17 @@
     // Do any additional setup after loading the view from its nib.
     self.navigationItem.title = @"自定义查询";
     
-    NSArray *arr = [[NSBundle mainBundle] loadNibNamed:@"LFFPickerVIew" owner:nil options:nil];
-    [self.view addSubview:arr[0]];
-
+   
     
+
+    pickerView = [LFFPickerVIew awakeFromXib];
+    pickerView.delegate = self;
+    pickerView.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+    [self.view addSubview:pickerView];
+    pickerView.alpha = 0;
+    
+    _startTImeL.text = [pickerView formatterDate:[NSDate date]];
+    _overTimeL.text = [pickerView formatterDate:[NSDate date]];
     
 }
 
@@ -43,15 +53,47 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - lffpickerviewdelegate
+-(void)changeAlphaHiden{
+    
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        pickerView.alpha = 0;
+    }];
+}
+-(void)changeAlphaHiden:(NSString *)dateStr{
+    
+    if (pickerView.Timetype ==1) {
+        _startTImeL.text = dateStr;
+    }
+    if (pickerView.Timetype == 2) {
+        _overTimeL.text = dateStr;
+    }
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        pickerView.alpha = 0;
+    }];
+}
+
+
+
+
 
 //开始时间
 - (IBAction)startTimeBtn:(id)sender {
-
+pickerView.Timetype = 1;
+    [UIView animateWithDuration:0.2 animations:^{
+        pickerView.alpha = 1;
+    }];
+    
     
 }
 //结束时间
 - (IBAction)overTimeBtn:(id)sender {
-    
+    pickerView.Timetype = 2;
+    [UIView animateWithDuration:0.2 animations:^{
+        pickerView.alpha = 1;
+    }];
     
 }
 
